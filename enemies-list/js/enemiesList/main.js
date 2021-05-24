6ca8dab45222e39921a20428c326d2e27a2eba63
Bryson Goins <brysonmg@gmail.com>
2021-05-24 14:21:21 -0500

    solved

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,47 @@
-// Put your code here
+console.log("My Enemies List!")
+console.log("----------------")
+
+const getEnemies = () => {
+    const enemies = []
+
+    const constructor = (first, last, offenses, hated) => {
+        return {
+            FirstName: first,
+            LastName: last,
+            Offenses: offenses,
+            IsReallyHated: hated
+        }
+    }
+
+    enemies.push(constructor("Joshua", "Flowers", [
+        "Being a jerk to me in elementary school",
+        "Not being nice to me in elementary school"
+    ], true))
+    enemies.push(constructor("Darth", "Vader", [
+        "Cut off Luke's hand",
+        "Murdered all those kids",
+        "Unkind management practices"
+    ], false))
+    enemies.push(constructor("Betty", "Rudelady", [
+        "Phone calls in the theater",
+        "Phone calls on the bus",
+        "Phone calls in line at the grocery store",
+        "Poor conversationalist"
+    ], true))
+    enemies.push(constructor("Leon", "Peck", [
+        "Keeps giving me a hotplate"
+    ], false))
+
+    return enemies
+}
+
+const enemies = getEnemies()
+
+for (const enemy of enemies) {
+    if (enemy.IsReallyHated) {
+        console.log(`${enemy.FirstName} ${enemy.LastName} (Really, really dislike!)`)
+    } else {
+        console.log(`${enemy.FirstName} ${enemy.LastName}`)
+    }
+}
+
