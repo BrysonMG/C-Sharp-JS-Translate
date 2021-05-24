84da131fbc2efbc593aff7a5aed6ac5a857a5476
Bryson Goins <brysonmg@gmail.com>
2021-05-24 13:49:26 -0500

    solved

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,18 @@
-// Put your code here
+const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+console.log("All Place Names")
+
+for (const name of names) {
+    console.log(name)
+}
+
+console.log()
+
+const theNames = names.filter(name => name.startsWith("The"))
+
+console.log("'The' Place Names")
+
+for (const name of theNames) {
+    console.log(name)
+}
